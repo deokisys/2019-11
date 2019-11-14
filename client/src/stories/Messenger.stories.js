7fedc3d5a119ae197fb5d3713d6a1b626bfed893
deokisys <sjb378@me.com>
2019-11-14 14:32:31 +0900

    Add messenger create button to storybook
    메신저 생성 버튼을 스토리북에 추가하였다.

---
+++ client/src/stories/Messenger.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MessengerCreateButton from '../components/Messenger/MessengerCreateButton';
+
+export default {
+  title: 'Messenger',
+};
+
+export const CreateButton = () => (
+    <>
+    <MessengerCreateButton>
+    </MessengerCreateButton>
+    </>
+);
